4e22167a57b1b2147213dad98b65bd6d970ac186
Amro234 <mamr5406@gmail.com>
2025-12-20 16:00:35 +0200

    First Part of Server UI is Finished -> Go Shaltot

--- Server/src/main/java/module-info.java
+++ Server/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.mycompany.server {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.base;
 
     opens com.mycompany.server to javafx.fxml;
     exports com.mycompany.server;
